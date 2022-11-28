a3a2ec623f8fc223ce4597f98f26cd8aeeedde9c
Avi Konduru <avikonduru@gmail.com>
2022-11-28 20:34:12 +0000

    added routes

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
+import pandas as pd
+import numpy as np
+import openai
+from openai.embeddings_utils import get_embedding, cosine_similarity
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return 'Hello from Flask!'
-
-
+	if request.method == 'POST':
+		return 'Calling the ChairmanAI Python GET route'
+	else:
+		df = pd.read_csv('wiki_set.csv')
+		df["babbage_search"] = df.babbage_search.apply(eval).apply(np.array)
+		print(df)
+		return 'Calling the ChairmanAI Python POST route'
+			
 app.run(host='0.0.0.0', port=81)
